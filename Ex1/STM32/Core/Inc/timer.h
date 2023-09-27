b4d649cf701ea2d1f44680fa81a7756883b7ad7a
duvanan13 <duvananproxxx2@gmail.com>
2023-09-27 10:37:32 +0700

    Init

---
+++ Ex1/STM32/Core/Inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: 7 Sep 2023
+ *      Author: duvan
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+
+
+#endif /* INC_TIMER_H_ */
---
+++ Ex1/STM32/Core/Src/timer.c
@@ -0,0 +1,8 @@
+/*
+ * timer.c
+ *
+ *  Created on: 7 Sep 2023
+ *      Author: duvan
+ */
+
+
